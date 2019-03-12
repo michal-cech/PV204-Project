129804f7d4482deea1a6923d9a8f714969d20dcc
BulakM <32273398+BulakM@users.noreply.github.com>
2019-03-12 20:44:58 +0100

    Vygenerování heatmap
    Heatmapy

--- heatmap.py
+++ heatmap.py
@@ -7,9 +7,9 @@ from pprint import pprint
 def get_data_rsa(file):
     csv_reader = csv.reader(open(file), delimiter=';')
     temp_d_msb = []
-    d_msb = [95]
-    t = [0]
-    occ = [0]
+    d_msb = []
+    t = []
+    occ = []
 
     for row in csv_reader:
         temp_msb = get_msb(row[5])
@@ -33,12 +33,12 @@ def get_data_rsa(file):
 def get_data_ecc(file):
     csv_reader = csv.reader(open(file), delimiter=';')
     temp_d_msb = []
-    d_msb = [95]
-    t = [0]
-    occ = [0]
+    d_msb = []
+    t = []
+    occ = []
 
     for row in csv_reader:
-        temp_msb = get_msb(get_msb(row[2]))
+        temp_msb = get_msb(row[2])
         temp_t = round(get_time(row[3]) / 100000)
 
         d_msb_index = next((index for (index, d) in enumerate(temp_d_msb) if d["msb"] == temp_msb and d["t"] == temp_t), None)
@@ -70,5 +70,5 @@ def get_time(str_time):
 
 ##get_data_rsa('rsa1024.txt')
 ##get_data_rsa('rsa2048.txt')
-##get_data_rsa('rsa512.txt')
-get_data_ecc('result.csv')
+get_data_rsa('rsa512.txt')
+##get_data_ecc('result.csv')
