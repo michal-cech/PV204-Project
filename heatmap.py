c7a10ba508629b263ee22d6139d5d8c914f1921d
BulakM <32273398+BulakM@users.noreply.github.com>
2019-03-13 18:11:14 +0100

    přidání modulus heatmap
    modulus heatmapy

--- heatmap.py
+++ heatmap.py
@@ -32,6 +32,34 @@ def get_data_rsa(file):
 	"layout" : go.Layout(autosize=True, plot_bgcolor='#ffffff', title= file[:-4].upper() + " private key MSB and time generation heatmap", xaxis=dict(title="Private key MSB value"), yaxis=dict(title="Keygen time(ms)"))},
 	filename= file[:-4] + '_heat_d_msb.html')
 
+def get_data_rsa_modulus(file):
+    csv_reader = csv.reader(open(file), delimiter=';')
+    temp_n_msb = []
+    n_msb = []
+    t = []
+    occ = []
+
+    for row in csv_reader:
+        temp_msb = get_msb(row[1])
+        temp_t = round(get_time(row[6]) // 1000000)
+
+        n_msb_index = next((index for (index, d) in enumerate(temp_n_msb) if d["msb"] == temp_msb and d["t"] == temp_t), None)
+
+        if n_msb_index:
+            temp_n_msb[n_msb_index]['occ'] += 1
+        else:
+            temp_n_msb.append({'msb' : temp_msb, 't' : temp_t, 'occ' : 1})
+
+    for i in range(len(temp_n_msb)):
+        n_msb.append(temp_n_msb[i]['msb'])
+        t.append(temp_n_msb[i]['t'])
+        occ.append(temp_n_msb[i]['occ'])
+
+
+    plot({"data" : [go.Heatmap(z=occ, x=n_msb, y=t)],
+	"layout" : go.Layout(autosize=True, plot_bgcolor='#ffffff', title= file[:-4].upper() + " private key MSB and time generation heatmap", xaxis=dict(title="Private key MSB value"), yaxis=dict(title="Keygen time(ms)"))},
+	filename= file[:-4] + '_heat_n_msb.html')
+
 def get_data_ecc(file):
     csv_reader = csv.reader(open(file), delimiter=';')
     temp_d_msb = []
@@ -71,8 +99,10 @@ def get_lsb(str_number):
 def get_time(str_time):
     return float(str_time[:-3])
 
-
-##get_data_rsa('rsa1024.txt')
+##get_data_rsa_modulus('rsa2048.txt')
+##get_data_rsa_modulus('rsa1024.txt')
+get_data_rsa_modulus('rsa512.txt')
 ##get_data_rsa('rsa2048.txt')
+##get_data_rsa('rsa1024.txt')
 ##get_data_rsa('rsa512.txt')
-get_data_ecc('result.csv')
+##get_data_ecc('result.csv')
