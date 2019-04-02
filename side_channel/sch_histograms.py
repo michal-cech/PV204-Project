364fb607655e1d82e1037c115675348f30ec16c3
BulakM <32273398+BulakM@users.noreply.github.com>
2019-04-02 11:03:14 +0200

    Oprava grafů

--- side_channel/sch_histograms.py
+++ side_channel/sch_histograms.py
@@ -18,7 +18,7 @@ def get_graphs_time(file):
         id.append(row[0])
         t.append(get_time(row[1]) / 1000000)
 
-    plot({'data': [go.Histogram(x=t, nbinsx = 256, autobinx = False)],
+    plot({'data': [go.Histogram(x=t)],
          'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (time)', xaxis=dict(title='Value of time(ms)'), yaxis=dict(title='Number of occurrences'))},
          filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_time.html')
 
@@ -41,15 +41,15 @@ def get_graphs_hw_time(file):
         hw.append(row[1])
         t.append(get_time(row[2]) / 1000000)
 
-    plot({'data': [go.Histogram(x=hw, nbinsx = 256, autobinx = False)],
+    plot({'data': [go.Histogram(x=hw)],
          'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (hamming weight)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Number of occurrences'))},
          filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hw.html')
 
-    plot({'data': [go.Histogram(x=t, nbinsx = 256, autobinx = False)],
+    plot({'data': [go.Histogram(x=t)],
          'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (time)', xaxis=dict(title='Value of time(ms)'), yaxis=dict(title='Number of occurrences'))},
          filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_time.html')
 
-    plot({'data': [go.Histogram2d(x=hw, y=t, nbinsx = 256, autobinx = False)],
+    plot({'data': [go.Histogram2d(x=hw, y=t)],
         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram2d - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time / occurences)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Value of time(ms)'))},
         filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hist2d.html')
 
@@ -74,15 +74,15 @@ def get_graphs_hw_length_time(file):
         length.append(row[2])
         t.append(get_time(row[3]) / 1000000)
 
-    plot({'data': [go.Histogram(x=hw, nbinsx = 256, autobinx = False)],
+    plot({'data': [go.Histogram(x=hw)],
         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (hamming weight)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Number of occurrences'))},
         filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hw.html')
 
-    plot({'data': [go.Histogram(x=t, nbinsx = 256, autobinx = False)],
+    plot({'data': [go.Histogram(x=t)],
         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (time)', xaxis=dict(title='Value of time(ms)'), yaxis=dict(title='Number of occurrences'))},
         filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_time.html')
 
-    plot({'data': [go.Histogram2d(x=hw, y=t, nbinsx = 256, autobinx = False)],
+    plot({'data': [go.Histogram2d(x=hw, y=t)],
         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram2d - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time / occurences)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Value of time(ms)'))},
         filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hist2d.html')
 
