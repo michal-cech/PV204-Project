121ea4176d50fc7996b4ad74c6611fea9f00a9b4
BulakM <32273398+BulakM@users.noreply.github.com>
2019-04-02 10:19:54 +0200

    Dokončení grafů

--- side_channel/sch_histograms.py
+++ side_channel/sch_histograms.py
@@ -1,8 +1,30 @@
 from plotly.offline import plot
 import plotly.graph_objs as go
+import operator
 from pprint import pprint
 import csv
 import math
+
+def get_graphs_time(file):
+    csv_reader = csv.reader(open(file), delimiter=';')
+    id = []
+    t = []
+
+    sorted_reader = sorted(csv_reader, key=operator.itemgetter(1))
+
+    for row in sorted_reader[:-1000]:
+        if row[0] == "ID":
+            continue
+        id.append(row[0])
+        t.append(get_time(row[1]) / 1000000)
+
+    plot({'data': [go.Histogram(x=t, nbinsx = 256, autobinx = False)],
+         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (time)', xaxis=dict(title='Value of time(ms)'), yaxis=dict(title='Number of occurrences'))},
+         filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_time.html')
+
+    plot({'data': [go.Bar(x=id, y=t)],
+         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' bar - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time)', xaxis=dict(title='Index'), yaxis=dict(title='Value of time(ms)'))},
+         filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_bar.html')
 
 def get_graphs_hw_time(file):
     csv_reader = csv.reader(open(file), delimiter=';')
@@ -10,7 +32,9 @@ def get_graphs_hw_time(file):
     hw = []
     t = []
 
-    for row in csv_reader:
+    sorted_reader = sorted(csv_reader, key=operator.itemgetter(2))
+
+    for row in sorted_reader[:-1000]:
         if row[0] == "ID":
             continue
         id.append(row[0])
@@ -18,28 +42,68 @@ def get_graphs_hw_time(file):
         t.append(get_time(row[2]) / 1000000)
 
     plot({'data': [go.Histogram(x=hw, nbinsx = 256, autobinx = False)],
-         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' hist - Random exponent (hamming weight)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Number of occurrences'))},
+         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (hamming weight)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Number of occurrences'))},
          filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hw.html')
 
     plot({'data': [go.Histogram(x=t, nbinsx = 256, autobinx = False)],
-         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' hist - Random exponent (time)', xaxis=dict(title='Value of time(ms)'), yaxis=dict(title='Number of occurrences'))},
+         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (time)', xaxis=dict(title='Value of time(ms)'), yaxis=dict(title='Number of occurrences'))},
          filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_time.html')
 
     plot({'data': [go.Histogram2d(x=hw, y=t, nbinsx = 256, autobinx = False)],
-         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' heat - Random exponent (hw/ time / occurences)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Value of time(ms)'))},
-         filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_heat.html')
+        'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram2d - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time / occurences)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Value of time(ms)'))},
+        filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hist2d.html')
 
     plot({'data': [go.Bar(x=id, y=t)],
-         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' bar - Random exponent (hw / time)', xaxis=dict(title='Value of id'), yaxis=dict(title='Value of time(ms)'))},
+         'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' bar - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time)', xaxis=dict(title='Index'), yaxis=dict(title='Value of time(ms)'))},
          filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_bar.html')
+
+def get_graphs_hw_length_time(file):
+    csv_reader = csv.reader(open(file), delimiter=';')
+    id = []
+    length = []
+    hw = []
+    t = []
+
+    sorted_reader = sorted(csv_reader, key=operator.itemgetter(3))
+
+    for row in sorted_reader[:-1000]:
+        if row[0] == "ID":
+         continue
+        id.append(row[0])
+        hw.append(row[1])
+        length.append(row[2])
+        t.append(get_time(row[3]) / 1000000)
+
+    plot({'data': [go.Histogram(x=hw, nbinsx = 256, autobinx = False)],
+        'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (hamming weight)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Number of occurrences'))},
+        filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hw.html')
+
+    plot({'data': [go.Histogram(x=t, nbinsx = 256, autobinx = False)],
+        'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram - ' + file[4:-4].replace("_", " ").capitalize() + ' (time)', xaxis=dict(title='Value of time(ms)'), yaxis=dict(title='Number of occurrences'))},
+        filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_time.html')
+
+    plot({'data': [go.Histogram2d(x=hw, y=t, nbinsx = 256, autobinx = False)],
+        'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' histogram2d - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time / occurences)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Value of time(ms)'))},
+        filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_hist2d.html')
+
+    plot({'data': [go.Heatmap(x=hw, y=t, z=length)],
+        'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' heatmap - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time / length)', xaxis=dict(title='Value of hamming weight'), yaxis=dict(title='Value of time(ms)'))},
+        filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_heat.html')
+
+    plot({'data': [go.Bar(x=id, y=t)],
+        'layout': go.Layout(autosize=True, plot_bgcolor='#ffffff', title=file[0:3].upper() + ' bar - ' + file[4:-4].replace("_", " ").capitalize() + ' (hw / time)', xaxis=dict(title='Index'), yaxis=dict(title='Value of time(ms)'))},
+        filename= 'graphs/' + file[0:3] + '/' + file[:-4] + '_bar.html')
 
 def get_time(str_time):
     return float(str_time[:-3])
 
-##get_graphs('rsa_random_message_sig.txt')
-##get_graphs('rsa_random_exp_sig.txt')
-##get_graphs('rsa_high_hw.txt')
-##get_graphs('rsa_random_msg_dec.txt')
-get_graphs_hw_time('rsa_random_exp_dec.txt')
-get_graphs_hw_time('ecc_random_messages.txt')
-get_graphs_hw_time('ecc_random_exp.txt')
+def sort_time(val):
+    return val[1]
+
+get_graphs_hw_length_time('rsa_random_message_sign.txt')
+get_graphs_hw_time('rsa_random_exponent_sign.txt')
+get_graphs_time('rsa_high_hamming_weight.txt')
+get_graphs_hw_length_time('rsa_random_message_decrypt.txt')
+get_graphs_hw_time('rsa_random_exponent_decrypt.txt')
+get_graphs_hw_time('ecc_random_message.txt')
+get_graphs_hw_time('ecc_random_exponent.txt')
